f112de9567cc32164e0b58c8851d921e732d84d1
Ka Wing Tsang <kawingt2@gmail.com>
2021-10-26 11:24:22 +0800

    Forms and CSRF token

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,14 @@
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 
-tasks = ['foo', 'bar', 'baz']
+tasks = []
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
 
 # Create your views here.
 
@@ -8,4 +16,21 @@ tasks = ['foo', 'bar', 'baz']
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
+    })
+
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
     })
